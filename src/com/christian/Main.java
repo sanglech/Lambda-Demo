843da58ddda0012207d82e22c50c9ab329467667
Christian <christian@sanglech.com>
2021-05-22 11:43:04 +0900

    Added sort empoyee demo using lambda

--- src/com/christian/Main.java
+++ src/com/christian/Main.java
@@ -1,5 +1,9 @@
 package com.christian;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
     public static void main(String []args){
 
@@ -15,14 +19,55 @@ public class Main {
         }).start();
         */
 
-        new Thread(()->System.out.println("Printing from runnable, lambda")).start();
+        //if one line no semi colon
+        new Thread(()->{
+            System.out.println("Line 1");
+            System.out.println("Line 2");
+            System.out.println("Line 3");
+            System.out.println("Line 4");
+        }).start();
+
+        Employee emp1= new Employee("mark",21);
+        Employee emp2= new Employee("john",31);
+        Employee emp3= new Employee("christian",41);
+
+        List<Employee> employees= new ArrayList<>();
+
+        employees.add(emp1);
+        employees.add(emp2);
+        employees.add(emp3);
+
+        //Sort employees
+/*        Collections.sort(employees, new Comparator<Employee>() {
+            @Override
+            public int compare(Employee o1, Employee o2) {
+                return o1.getName().compareTo(o2.getName());
+            }
+        });*/
+
+
+        //Sort Employee using Lambda
+
+        Collections.sort(employees,(e1,e2)->
+                e1.getName().compareTo(e2.getName())
+                );
+
+        //Print Employees
+
+        for(Employee e: employees){
+            System.out.println(e.getName());
+        }
+
+
 
     }
 }
+
+/*
 class CodeToRun implements Runnable{
 
     @Override
     public void run() {
         System.out.println("Printing from the Runnable");
     }
-}
+}*/
---
+++ src/com/christian/UpperConcat.java
@@ -0,0 +1,5 @@
+package com.christian;
+
+public interface UpperConcat {
+    public String upperConcat(String s1, String s2);
+}
