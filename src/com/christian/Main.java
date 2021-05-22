d4df5e79385c09a22944e241ef55425ba45eeecf
Christian <christian@sanglech.com>
2021-05-22 11:57:41 +0900

    Silly string demo with lambda

--- src/com/christian/Main.java
+++ src/com/christian/Main.java
@@ -39,12 +39,19 @@ public class Main {
             System.out.println(e.getName());
         }
 
-        String sillyString=doStringStuff(new UpperConcat() {
+        //Lambda definiton
+        UpperConcat uc=(s1,s2) -> (s1.toUpperCase()+s2.toUpperCase());
+
+        String sillyString=doStringStuff(uc,employees.get(0).getName(),employees.get(1).getName());
+
+/*        String sillyString=doStringStuff(new UpperConcat() {
             @Override
             public String upperConcat(String s1, String s2) {
                 return s1.toUpperCase() + s2.toUpperCase();
             }
-        }, employees.get(0).getName(),employees.get(1).getName());
+        }, employees.get(0).getName(),employees.get(1).getName());*/
+
+
 
         System.out.println(sillyString);
     }
